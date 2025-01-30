094fcbece3ab0cfad6510364aa3a658aa826b3f9
Ankit Kumar Panda <105581787+ankit-1k@users.noreply.github.com>
2025-01-30 15:32:56 +0530

    Update loader.component.ts

--- src/app/Common/loader/loader.component.ts
+++ src/app/Common/loader/loader.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent implements OnInit {
-
+  a:string=''
   constructor() { }
 
   ngOnInit(): void {
